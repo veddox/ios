792576081143960d3272f1f27f1bd6a3d528632e
Marino Faggiana <m.faggiana@twsweb.it>
2018-04-16 10:41:25 +0200

    dev

--- PickerFileProvider/FileProviderItem.swift
+++ PickerFileProvider/FileProviderItem.swift
@@ -15,23 +15,41 @@ class FileProviderItem: NSObject, NSFileProviderItem {
 
     var fileManager = FileManager()
 
-    var itemIdentifier: NSFileProviderItemIdentifier
-    var parentItemIdentifier: NSFileProviderItemIdentifier
-    
-    var capabilities: NSFileProviderItemCapabilities {
+    // Providing Required Properties
+    var itemIdentifier: NSFileProviderItemIdentifier                // The item's persistent identifier
+    var filename: String = ""                                       // The item's filename
+    var typeIdentifier: String = ""                                 // The item's uniform type identifiers
+    var capabilities: NSFileProviderItemCapabilities {              // The item's capabilities
         return .allowsAll
     }
     
-    var filename: String = ""
-    var typeIdentifier: String = ""
-    var childItemCount: NSNumber?
+    // Managing Content
+    var childItemCount: NSNumber?                                   // The number of items contained by this item
+    var documentSize: NSNumber?                                     // The document's size, in bytes
+
+    // Specifying Content Location
+    var parentItemIdentifier: NSFileProviderItemIdentifier          // The persistent identifier of the item's parent folder
+    var isTrashed: Bool = false                                     // A Boolean value that indicates whether an item is in the trash
+   
+    // Tracking Usage
+    var contentModificationDate: Date?                              // The date the item was last modified
+    var creationDate: Date?                                         // The date the item was created
+    var lastUsedDate: Date?                                         // The date the item was last used
+
+    // Tracking Versions
+    var versionIdentifier: Data?                                    // A data value used to determine when the item changes
+    var isMostRecentVersionDownloaded: Bool = true                  // A Boolean value that indicates whether the item is the most recent version downloaded from the server
+
+    // Monitoring File Transfers
+    var isUploading: Bool = false                                   // A Boolean value that indicates whether the item is currently uploading to your remote server
+    var isUploaded: Bool = true                                     // A Boolean value that indicates whether the item has been uploaded to your remote server
+    var uploadingError: Error?                                      // An error that occurred while uploading to your remote server
+    var isDownloading: Bool = false                                 // A Boolean value that indicates whether the item is currently downloading from your remote server
+    var isDownloaded: Bool = true                                   // A Boolean value that indicates whether the item has been downloaded from your remote server
+    var downloadingError: Error?                                    // An error that occurred while downloading the item
+
+    // Nextcloud metadata
     var metadata = tableMetadata()
-    var isShared: Bool = false
-    var isDownloaded: Bool = true
-    var isMostRecentVersionDownloaded: Bool = true
-    var isUploaded: Bool = true
-    var versionIdentifier: Data?
-    var documentSize: NSNumber?
     
     init(metadata: tableMetadata, root: Bool) {
         
